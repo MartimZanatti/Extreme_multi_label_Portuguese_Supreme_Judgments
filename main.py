bb7e62ea191376e75a7df02f44b22264b4c4f38b
Martim Zanatti Silva <martim_zanatti_5@hotmail.com>
2023-07-06 17:09:54 +0100

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from creation_dataset import get_sum_and_descritores, create_descritores_dataset
-from prepare_data import create_descritores_list
-
-get_sum_and_descritores()
-
-#create_descritores_list()
-
-#create_descritores_dataset()
